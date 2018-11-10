b18d31becd9e873c36427c0261599500334d3102
unknown <ray_ramez@yahoo.com>
2018-11-10 15:43:26 -0800

    App is done

--- logic.js
+++ logic.js
@@ -11,36 +11,33 @@ $(document).ready(function () {
     }
 
     $("#addBtn").on("click", function () { //creating a button when click add
-        event.preventDefault();
-        topics = [];
-        var textVal = $("#btnName").val();
-        topics.push(textVal);
-        for (var i = 0; i < topics.length; i++) {
-            $("#buttons").append("<button class=btnAnimal >" + topics[i] + "</button>");
+        event.preventDefault(); //prevent submit btn to submit
+        topics = []; //reset my array
+        var textVal = $("#btnName").val().trim(); //storing val that in btn to textVal
+        topics.push(textVal); // push that val to the array
+        for (var i = 0; i < topics.length; i++) { //created a loop that will take all inside the array and display them 
+            $("#buttons").append("<button class=btnAnimal >" + topics[i] + "</button>"); //appending each created btn to the div with id buttons
         }
-        renderButtons();
-        $("#btnName").focus();
+        renderButtons(); //calling func
+        $("#btnName").focus(); //sending cursor to the text input
 
     });
 
     //main function which will grap what is in the button and search with images related to it
     function renderButtons() {
-        $(".btnAnimal").on("click", function renderButtons(event) {
-            $("#display").empty();
-            var theBtn = $(event.target).text();
-            var queryURL = URL + "api_key=" + API_KEY + "&q=" + theBtn;
-            console.log(queryURL);
+        $(".btnAnimal").on("click", function renderButtons(event) { //this func will listne to button with class btnAmimal 
+            $("#display").empty();//will make empty the display
+            var theBtn = $(event.target).text();//storing the name inside the button to theBtn so i can add it to the query url
+            var queryURL = URL + "api_key=" + API_KEY + "&q=" + theBtn; 
 
-            $.ajax({
+            $.ajax({ //ajax function 
                     url: queryURL,
                     method: "GET"
                 })
-                .then(function (response) {
-                    for (var j = 0; j < 5; j++) {
+                .then(function (response) { //creating the promise function
+                    for (var j = 0; j < 10; j++) { //loop over the number of limit to display the images
                         var ImgUrl = response.data[j].images.fixed_height_still.url;
                         var imgUrlFixed = response.data[j].images.fixed_height.url
-                        console.log(ImgUrl);
-                        console.log(imgUrlFixed);
                         var ratingApi = response.data[j].rating;
                         var img_tag = $("<img>");
                         img_tag.attr("src", ImgUrl);
@@ -50,31 +47,25 @@ $(document).ready(function () {
                         img_tag.attr("data-state", "still");
 
                         $("#display").append("<div><p>Rating is: " + ratingApi + "</p></div>");
-
                         $("#display").append(img_tag);
-                            // "<div>The Iamge: <img class=.gif src=" + ImgUrl + " data-still=" + ImgUrl + "data-animate=" + imgUrlFixed + "data-state= still ></div>");
-
-                        $(".gif").on("click", function () {
-                            var state = $(this).attr("data-state");
-                            if (state === "still") {
-                                $(this).attr("src", $(this).attr("data-animate"));
-                                $(this).attr("data-state", "animate");
-                            } else {
-                                $(this).attr("src", $(this).attr("data-still"));
-                                $(this).attr("data-state", "still");
-                            }
-                        });
-
-                        $("#display").append("=========================================================")
+                        $("#display").append("<br><br>")
                         $("#btnName").focus();
-
                     }
-
-
+                    $(".gif").on("click", function () { //the function will lisnt to the imamge click to play or stop
+                        var state = $(this).attr("data-state");
+                        console.log('state', state)
+                        if (state === "still") {
+                            $(this).attr("src", $(this).attr("data-animate"));
+                            $(this).attr("data-state", "animate");
+                        } else {
+                            $(this).attr("src", $(this).attr("data-still"));
+                            $(this).attr("data-state", "still");
+                        }
+                    });
                 })
         })
     };
-    renderButtons();
+    renderButtons(); 
     $("#btnName").focus();
 
 
